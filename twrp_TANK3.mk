6a4dbd8d5b1ce21005dd0b9a964ce84da9f8d51c
nino <pilopilosimon1990@gmail.com>
2026-01-12 16:58:08 +0100

    Update twrp_TANK3.mk

--- twrp_TANK3.mk
+++ twrp_TANK3.mk
@@ -5,8 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-$(call inherit-product, device/oblue/TANK3/patches.mk ) 
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
