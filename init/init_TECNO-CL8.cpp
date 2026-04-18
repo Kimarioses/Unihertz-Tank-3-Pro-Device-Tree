1be59e7039458f49b22a82baab2b62b25c8a21db
nino <pilopilosimon1990@gmail.com>
2026-04-18 22:41:46 +0100

    Update product properties for TECNO-CL8

--- init/init_TECNO-CL8.cpp
+++ init/init_TECNO-CL8.cpp
@@ -24,8 +24,8 @@ void vendor_load_properties()
     string prop_partitions[] = {"", "vendor.", "odm."};
     for (const string &prop : prop_partitions)
     {
-        property_override(string("ro.product.") + prop + string("name"), "CL8-OP");
-        property_override(string("ro.product.") + prop + string("marketname"), "TECNO CAMON 30 Pro");
-        property_override(string("ro.product.system.") + prop + string("device"), "TECNO CAMON 30 Pro");
+        property_override(string("ro.product.") + prop + string("name"), "TANK3-OP");
+        property_override(string("ro.product.") + prop + string("marketname"), "TANK3 Pro");
+        property_override(string("ro.product.system.") + prop + string("device"), "TANK3 Pro");
     }
 }
